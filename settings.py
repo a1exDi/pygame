d7a2947960b05b61266b78c37bf3e8cfc5e20e7b
Alexey Shirkin <alexsey.shirkin@gmail.com>
2022-02-10 19:01:03 +0300

    aproove

---
+++ settings.py
@@ -0,0 +1,29 @@
+LEVEL_MAP = [
+'                            ',
+'                            ',
+'                            ',
+'       XXXX           XX    ',
+'   P                        ',
+'XXXXX         XX         XX ',
+' XXXX       XX              ',
+' XX    X  XXXX    XX  XX    ',
+'       X  XXXX    XX  XXX   ',
+'    XXXX  XXXXXX  XX  XXXX  ',
+'XXXXXXXXXXXXXXXXXXXXXXXXXX  ']
+
+TILE_SIZE = 64
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+# colors 
+BG_COLOR = '#b5d7ff'
+PLAYER_COLOR = '#7e1e5c'
+TILE_COLOR = '#397e1e'
+
+# camera
+CAMERA_BORDERS = {
+	'left': 100,
+	'right': 200,
+	'top':100,
+	'bottom': 150
+}
